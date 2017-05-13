0634c080504178b797dcee5ae046b63688f7af1f
gihuny <nagihuny@naver.com>
2017-05-13 14:37:02 +0900

    Add files via upload

---
+++ picam_picture_342_347p_timestamp.py
@@ -0,0 +1,33 @@
+import picamera
+import time
+
+with picamera.PiCamera() as camera:
+    res = (int)(input('Select Resolution: 1:320x240, 2:640x480, 3:1024x768'))
+
+    if res is 3:
+        camera.resolution = (1024, 768)
+    elif res is 2:
+        camera.resolution = (640, 480)
+    elif res is 1:
+        camera.resolution = (320, 240)
+    else:
+        camera.resolution = (1024, 768)
+
+    eff = (int)(input('Select Effect: 1:None, 2:oilpaint, 3:negative'))
+
+    if eff is 3:
+        camera.image_effect = 'negative'
+    elif eff is 2:
+        camera.image_effect = 'oilpaint'
+    else:
+        camera.image_effect = 'none'
+
+    #filename = input("Enter Filename")
+    filename = "auto"
+
+    tm = time.localtime()
+
+    camera.start_preview()
+    time.sleep(1)
+    camera.stop_preview()
+    camera.capture(filename+(str)(tm.tm_year)+'_'+(str)(tm.tm_mon)+'_'+(str)(tm.tm_mday)+'_'+(str)(tm.tm_hour)+'_'+(str)(tm.tm_min)+'_'+(str)(tm.tm_sec)+".jpg")
---
+++ picam_record_349_354p.py
@@ -0,0 +1,40 @@
+import picamera
+import time
+
+with picamera.PiCamera() as camera:
+    res = (int)(input('Select Resolution: 1:320x240, 2:640x480, 3:1024x768'))
+
+    if res is 3:
+        camera.resolution = (1024, 768)
+    elif res is 2:
+        camera.resolution = (640, 480)
+    elif res is 1:
+        camera.resolution = (320, 240)
+    else:
+        camera.resolution = (1024, 768)
+
+    rot = (int)(input('Select Rotation: 1:90, 2:180, 3:270, else:No rotation'))
+
+    if rot is 3:
+        camera.rotation = 270
+    elif rot is 2:
+        camera.rotation = 180
+    elif rot is 1:
+        camera.rotation = 90
+
+    filename = input("Enter Filename")
+    
+#    camera.start_preview()
+#    time.sleep(1)
+#    camera.stop_preview()
+#    camera.capture(filename+".jpg")
+
+    camera.start_preview()
+    camera.start_recording(output = filename + '.h264')
+    camera.wait_recording(5)
+    camera.stop_preview()
+    camera.stop_recording()
+
+
+
+
