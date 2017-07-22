fbdf6277370bc28aafd2f85fa19a6d54be76c12b
gihuny <nagihuny@naver.com>
2017-07-22 09:32:14 +0900

    Add files via upload

---
+++ analog_tests/spi_book.py
@@ -0,0 +1,35 @@
+import spidev
+import time
+
+spi = spidev.SpiDev() # create SPI Class
+spi.open(0,0) # dev, port
+
+spi.max_speed_hz=100000
+spi.bits_per_word=8
+
+dummy = 0xff
+start = 0x47
+sgl = 0x20
+ch0 = 0x00
+ch1 = 0x10
+msbf = 0x8
+
+def measure(ch):
+    ad = spi.xfer2( [ (start | sgl | ch | msbf), dummy] )
+    val = (( ((ad[0] & 0x3) <<8) + ad[1] ) * 3.3) / 1023
+    print("ch: %d rddata ad[0]: %x, ad[1]: %x, ad: %4d, calval: %f" % (ch>>4, ad[0], ad[1],( ((ad[0] & 0x3) << 8) | ad[1]), val))
+
+    return val
+
+try:
+    while 1:
+        mes_ch0 = measure(ch0)
+        mes_ch1 = measure(ch1)
+        print("ch0 - %2.2f [V] ch1 - %2.2f [V]" % (mes_ch0 , mes_ch1))
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+
+spi.close()
